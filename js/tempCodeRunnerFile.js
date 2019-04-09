81ffdd5a259bee3f325ef056ab30f4b0e4895a05
Your Name <yananik3@gmail.com>
2019-04-09 13:13:26 +0300

    Initial commit

---
+++ js/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+const switcher = document.querySelector('#cbx'),
+      more = document.querySelector('.more'),
+      modal = document.querySelector('.modal'),
+      videos = document.querySelector('.videos__item');
+let player;
+
